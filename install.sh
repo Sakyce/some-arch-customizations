2a0712f3e4c7d3d8048bca00fcc30fc96e04e3ca
Sakyce <55030860+Sakyce@users.noreply.github.com>
2022-07-08 20:21:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
+# Copy the template
+cp xenia_ascii_color.txt ~/.config/xenia_ascii_color.txt
 
+# Write some stuff to .bashrc
+echo 'alias neofetch="neofetch --source /run/media/berry/DisqueDur/xenia_ascii_color.txt --ascii_colors 1 3 7 4"' >> ~/.bashrc
+echo 'alias clear="clear && neofetch"' >> ~/.bashrc
+echo 'neofetch' >> ~/.bashrc
